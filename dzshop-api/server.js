3a4a4a3d6c4e991a317d1b1ab1ba6c8692705944
nadhir <nadhirchougui3@gmail.com>
2026-09-25 23:36:20 +0100

    Update project

--- dzshop-api/server.js
+++ dzshop-api/server.js
@@ -10,7 +10,7 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use(cors({ origin: 'http://localhost:5179' }))
+app.use(cors({ origin: 'http://localhost:5175' }))
 app.use(express.json())
 
 app.use('/api/products', productRoutes)
